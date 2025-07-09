4a7e09fe2e3df88e9a643f868bf78cfc06c503d4
Ek-shakti <shaktieknist@gmail.com>
2025-07-10 03:55:07 +0530

    Add files via upload

---
+++ api.js
@@ -0,0 +1,31 @@
+fetch('https://dummyjson.com/products')
+    .then(Response => Response.json())
+    .then(characters => {
+        // renderProducts(characters.products)
+        console.log(characters)
+
+    });
+const cardContainer = document.querySelector('#div')
+function renderProducts(products) {
+    products.forEach(product => {
+
+        const div = document.createElement('div')
+        const image = document.createElement('img')
+        const price = document.createElement('h3')
+
+        div.classList = 'card'
+        image.classList = 'card-img'
+        price.classList = 'card-price'
+
+        price.textContent = `price: ${products.price}`
+
+        let c = image.setAttribute("src", products.images)
+
+        div.appendChild(image)
+        div.appendChild(price)
+        cardContainer.appendChild(div)
+
+    });
+
+}
+
