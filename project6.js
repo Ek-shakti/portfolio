427b0800cf3b7da4a0b3d853a697c8091f2e4818
Ek-shakti <shaktieknist@gmail.com>
2025-07-10 03:43:50 +0530

    Update project6.js

--- project6.js
+++ project6.js
@@ -1,34 +1,4 @@
 
-// const timer = new Date("30").getTime;
-
-// setInterval(() => {
-
-//     let now = new Date().getTime;
-//     let stop = timer.getTime() - now.getTime()
-
-//     // var minutes = Math.floor((stop % (1000 * 60 * 60)) / (1000 * 60));
-//     // var seconds = Math.floor((stop % (1000 * 60)) / 1000);
-
-//     if (stop < 0) {
-//         clearInterval()
-//         const t = document.querySelector("#timer").textContent = stop
-//     }
-
-// }, 1000);
-
-
-// let timeleft = "30"
-// setInterval(() => {
-//     // (`Time left -${timeleft}`);
-//     timeleft--;
-//     if (timeleft < 0) {
-//         clearInterval()
-//         const t = document.querySelector("#timer").textContent = timeleft
-//     }
-
-// }, 1000);
-
-
 let timeLeft = 30;
 const timer = setInterval(() => {
     const time = (`Time left -  00:${timeLeft}`);
