82cc6cab951466567ee03962c5c4f01eba6633e2
Ek-shakti <shaktieknist@gmail.com>
2025-08-14 00:34:21 +0530

    Update project8.js

--- project8.js
+++ project8.js
@@ -1,199 +1,51 @@
-const red1 = document.querySelector('#r1')
-const yellow1 = document.querySelector('#y1')
-const green1 = document.querySelector('#g1')
 
-const red2 = document.querySelector('#r2')
-const yellow2 = document.querySelector('#y2')
-const green2 = document.querySelector('#g2')
+const signals = [
+    ["r1", "y1", "g1"],
+    ["r2", "y2", "g2"],
+    ["r3", "y3", "g3"],
+    ["r4", "y4", "g4"]
+].map(([r, y, g]) => ({
+    red: document.getElementById(r),
+    yellow: document.getElementById(y),
+    green: document.getElementById(g)
+}));
 
-const red3 = document.querySelector('#r3')
-const yellow3 = document.querySelector('#y3')
-const green3 = document.querySelector('#g3')
+function setAllRed() {
+    signals.forEach(sig => {
+        sig.red.style.backgroundColor = "red";
+        sig.yellow.style.backgroundColor = "gray";
+        sig.green.style.backgroundColor = "gray";
+    });
+}
 
-const red4 = document.querySelector('#r4')
-const yellow4 = document.querySelector('#y4')
-const green4 = document.querySelector('#g4')
+async function runCycle(activeIndex, greenTime, yellowTime) {
+    // All signals start as red
+    setAllRed();
 
-// const greycolor = 'bg-gray-500'
+    // Green for active
+    signals[activeIndex].red.style.backgroundColor = "gray";
+    signals[activeIndex].green.style.backgroundColor = "green";
+    await new Promise(res => setTimeout(res, greenTime));
 
-// for (let index = 0; index < 100; index++) {
-//     console.log(index);
+    // Yellow for active
+    signals[activeIndex].green.style.backgroundColor = "gray";
+    signals[activeIndex].yellow.style.backgroundColor = "yellow";
+    await new Promise(res => setTimeout(res, yellowTime));
 
-//     setInterval(() => {
-//         green1.classList.replace('bg-gray-500', 'bg-green-500')
-//         yellow1.classList.replace('bg-yellow-500', 'bg-gray-500')
-//         red1.classList.replace('bg-red-500', 'bg-gray-500')
+    // Back to red for active
+    signals[activeIndex].yellow.style.backgroundColor = "gray";
+    signals[activeIndex].red.style.backgroundColor = "red";
+}
 
-//     }, 7000);
+async function startTraffic() {
+    const greenTime = 4000;
+    const yellowTime = 2000;
 
-//     setTimeout(() => {
+    while (true) {
+        for (let i = 0; i < signals.length; i++) {
+            await runCycle(i, greenTime, yellowTime);
+        }
+    }
+}
 
-//         yellow1.classList.replace('bg-gray-500', 'bg-yellow-500')
-//         red1.classList.replace('bg-red-500', 'bg-gray-500')
-//         green1.classList.replace('bg-green-500', 'bg-gray-500')
-
-//     }, 2000);
-
-//     setTimeout(() => {
-//         red1.classList.replace('bg-gray-500', 'bg-red-500')
-//         green1.classList.replace('bg-green-500', 'bg-gray-500')
-//         yellow1.classList.replace('bg-yellow-500', 'bg-gray-500')
-
-
-
-//     }, 5000);
-// }
-
-
-// for (let index = 0; index < 100; index++) {
-//     console.log(index);
-
-//     setInterval(() => {
-//         red2.classList.replace('bg-gray-500', 'bg-red-500')
-//         green2.classList.replace('bg-green-500', 'bg-gray-500')
-//         yellow2.classList.replace('bg-yellow-500', 'bg-gray-500')
-
-//     }, 5000);
-
-//     setTimeout(() => {
-
-//         yellow2.classList.replace('bg-gray-500', 'bg-yellow-500')
-//         red2.classList.replace('bg-red-500', 'bg-gray-500')
-//         green2.classList.replace('bg-green-500', 'bg-gray-500')
-
-//     }, 2000);
-
-//     setTimeout(() => {
-//         green2.classList.replace('bg-gray-500', 'bg-green-500')
-//         yellow2.classList.replace('bg-yellow-500', 'bg-gray-500')
-//         red2.classList.replace('bg-red-500', 'bg-gray-500')
-
-//     }, 7000);
-// }
-
-
-
-// for (let index = 0; index < 100; index++) {
-//     console.log(index);
-
-//     setInterval(() => {
-//         red3.classList.replace('bg-gray-500', 'bg-red-500')
-//         green3.classList.replace('bg-green-500', 'bg-gray-500')
-//         yellow3.classList.replace('bg-yellow-500', 'bg-gray-500')
-
-//     }, 5000);
-
-//     setTimeout(() => {
-
-//         yellow3.classList.replace('bg-gray-500', 'bg-yellow-500')
-//         red3.classList.replace('bg-red-500', 'bg-gray-500')
-//         green3.classList.replace('bg-green-500', 'bg-gray-500')
-
-//     }, 2000);
-
-//     setTimeout(() => {
-//         green3.classList.replace('bg-gray-500', 'bg-green-500')
-//         yellow3.classList.replace('bg-yellow-500', 'bg-gray-500')
-//         red3.classList.replace('bg-red-500', 'bg-gray-500')
-
-//     }, 7000);
-// }
-
-
-// for (let index = 0; index < 100; index++) {
-//     console.log(index);
-
-//     setInterval(() => {
-//         red4.classList.replace('bg-gray-500', 'bg-red-500')
-//         green4.classList.replace('bg-green-500', 'bg-gray-500')
-//         yellow4.classList.replace('bg-yellow-500', 'bg-gray-500')
-
-//     }, 5000);
-
-//     setTimeout(() => {
-
-//         yellow4.classList.replace('bg-gray-500', 'bg-yellow-500')
-//         red4.classList.replace('bg-red-500', 'bg-gray-500')
-//         green4.classList.replace('bg-green-500', 'bg-gray-500')
-
-//     }, 2000);
-
-//     setTimeout(() => {
-//         green4.classList.replace('bg-gray-500', 'bg-green-500')
-//         yellow4.classList.replace('bg-yellow-500', 'bg-gray-500')
-//         red4.classList.replace('bg-red-500', 'bg-gray-500')
-
-//     }, 7000);
-// }
-
-
-setInterval(() => {
-    yellow1.classList.replace('bg-gray-500', 'bg-yellow-500')
-    yellow2.classList.replace('bg-gray-500', 'bg-yellow-500')
-    yellow3.classList.replace('bg-gray-500', 'bg-yellow-500')
-    yellow4.classList.replace('bg-gray-500', 'bg-yellow-500')
-
-}, 2000);
-
-
-setInterval(() => {
-
-    green1.classList.replace('bg-gray-500', 'bg-green-500')
-    red2.classList.replace('bg-gray-500', 'bg-red-500')
-    red3.classList.replace('bg-gray-500', 'bg-red-500')
-    red4.classList.replace('bg-gray-500', 'bg-red-500')
-    green2.classList.replace('bg-green-500', 'bg-gray-500')
-    green3.classList.replace('bg-green-500', 'bg-gray-500')
-    green4.classList.replace('bg-green-500', 'bg-gray-500')
-    yellow1.classList.replace('bg-yellow-500', 'bg-gray-500')
-    yellow2.classList.replace('bg-yellow-500', 'bg-gray-500')
-    yellow3.classList.replace('bg-yellow-500', 'bg-gray-500')
-    yellow4.classList.replace('bg-yellow-500', 'bg-gray-500')
-
-}, 4000);
-
-setInterval(() => {
-    green2.classList.replace('bg-gray-500', 'bg-green-500')
-    green1.classList.replace('bg-green-500', 'bg-gray-500')
-    red2.classList.replace('bg-red-500', 'bg-gray-500')
-    red1.classList.replace('bg-gray-500', 'bg-red-500')
-    red3.classList.replace('bg-gray-500', 'bg-red-500')
-    red4.classList.replace('bg-gray-500', 'bg-red-500')
-    yellow1.classList.replace('bg-yellow-500', 'bg-gray-500')
-    yellow2.classList.replace('bg-yellow-500', 'bg-gray-500')
-    yellow3.classList.replace('bg-yellow-500', 'bg-gray-500')
-    yellow4.classList.replace('bg-yellow-500', 'bg-gray-500')
-
-}, 6000);
-
-
-setTimeout(() => {
-    red1.classList.replace('bg-gray-500', 'bg-red-500')
-    red2.classList.replace('bg-gray-500', 'bg-red-500')
-    red4.classList.replace('bg-gray-500', 'bg-red-500')
-    green3.classList.replace('bg-gray-500', 'bg-green-500')
-    green1.classList.replace('bg-green-500', 'bg-gray-500')
-    green2.classList.replace('bg-green-500', 'bg-gray-500')
-    red3.classList.replace('bg-red-500', 'bg-gray-500')
-
-    yellow1.classList.replace('bg-yellow-500', 'bg-gray-500')
-    yellow2.classList.replace('bg-yellow-500', 'bg-gray-500')
-    yellow3.classList.replace('bg-yellow-500', 'bg-gray-500')
-    yellow4.classList.replace('bg-yellow-500', 'bg-gray-500')
-
-}, 12000);
-
-
-setTimeout(() => {
-    red1.classList.replace('bg-gray-500', 'bg-red-500')
-    red2.classList.replace('bg-gray-500', 'bg-red-500')
-    red3.classList.replace('bg-gray-500', 'bg-red-500')
-    green4.classList.replace('bg-gray-500', 'bg-green-500')
-    red4.classList.replace('bg-red-500', 'bg-gray-500')
-    yellow1.classList.replace('bg-yellow-500', 'bg-gray-500')
-    yellow2.classList.replace('bg-yellow-500', 'bg-gray-500')
-    yellow3.classList.replace('bg-yellow-500', 'bg-gray-500')
-    yellow4.classList.replace('bg-yellow-500', 'bg-gray-500')
-
-}, 14000);
-
+startTraffic();
