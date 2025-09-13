94c8594019f4df7460653ca28dc35d5e466aa606
Ek-shakti <shaktieknist@gmail.com>
2025-09-13 14:32:41 +0530

    Update project4.js

--- project4.js
+++ project4.js
@@ -1,24 +1,79 @@
 
+const currDisplay = document.querySelector(".curr-display");
+const prevDisplay = document.querySelector(".prev-display");
+const numbers = document.querySelectorAll(".number");
+const operands = document.querySelectorAll(".operation");
+const clearBtn = document.querySelector(".clear");
+const delBtn = document.querySelector(".delete");
+const equalBtn = document.querySelector(".equal");
+let operation;
 
-const display = document.getElementById('display');
+function appendNumber(number) {
+    if (number === "." && currDisplay.innerText.includes(".")) return;
+    currDisplay.innerText += number;
+}
 
-function appendValue(value) {
-    display.value += value;
+function chooseOperation(operand) {
+    if (currDisplay.innerText === "") return;
+    compute(operand);
+    operation = operand;
+    currDisplay.innerText += operand;
+    prevDisplay.innerText = currDisplay.innerText;
+    currDisplay.innerText = "";
 }
 
 function clearDisplay() {
-    display.value = '';
+    currDisplay.innerText = "";
+    prevDisplay.innerText = "";
 }
 
-function deleteLast() {
-    display.value = display.value.slice(0, -1);
-}
+function compute(operand) {
+    let result;
+    const previousValue = parseFloat(prevDisplay.innerText);
+    const currentValue = parseFloat(currDisplay.innerText);
 
-function calculate() {
-    try {
-        display.value = eval(display.value.replace('%', '/100'));
-    } catch {
-        display.value = 'Error';
+    if (isNaN(previousValue) || isNaN(currentValue)) return;
+
+    switch (operation) {
+        case "+":
+            result = previousValue + currentValue;
+            break;
+        case "-":
+            result = previousValue - currentValue;
+            break;
+        case "*":
+            result = previousValue * currentValue;
+            break;
+        case "/":
+            result = previousValue / currentValue;
+            break;
+        default:
+            return;
     }
+    currDisplay.innerText = result;
 }
 
+numbers.forEach((number) => {
+    number.addEventListener("click", () => {
+        appendNumber(number.innerText);
+    });
+});
+
+operands.forEach((operand) => {
+    operand.addEventListener("click", () => {
+        chooseOperation(operand.innerText);
+    });
+});
+
+clearBtn.addEventListener("click", () => {
+    clearDisplay();
+});
+
+equalBtn.addEventListener("click", () => {
+    compute();
+    prevDisplay.innerText = "";
+});
+
+delBtn.addEventListener("click", () => {
+    currDisplay.innerText = currDisplay.innerText.slice(0, -1);
+});
